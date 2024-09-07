d9fb4693780c8e59665f1526b1a916983f0e4f6f
Matus Benus <matusbenus02@gmail.com>
2024-09-07 18:58:23 +0200

    another tests for Queue Items Controller and refactored for newest .net 8 version

--- LibraryAppWebAPI/Controllers/BooksController.cs
+++ LibraryAppWebAPI/Controllers/BooksController.cs
@@ -6,128 +6,127 @@ using LibraryAppWebAPI.Models;
 using LibraryAppWebAPI.Models.DTOs;
 using LibraryAppWebAPI.Repository.Interfaces;
 
-namespace LibraryAppWebAPI.Controllers
+namespace LibraryAppWebAPI.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+[SwaggerTag("Books")]
+public class BooksController(IBookRepository bookRepository, IRentalEntryRepository rentalEntryRepository) : ControllerBase
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    [SwaggerTag("Books")]
-    public class BooksController(IBookRepository bookRepository, IRentalEntryRepository rentalEntryRepository) : ControllerBase
+    // GET: api/Books
+    [HttpGet]
+    [ProducesResponseType(200, Type = typeof(OkResult))]
+    [ProducesResponseType(404, Type = typeof(NotFound))]
+    [SwaggerOperation(Summary = "Get all books", Tags = ["Books"])]
+    public ActionResult<IEnumerable<Book>> GetBooks()
     {
-        // GET: api/Books
-        [HttpGet]
-        [ProducesResponseType(200, Type = typeof(OkResult))]
-        [ProducesResponseType(404, Type = typeof(NotFound))]
-        [SwaggerOperation(Summary = "Get all books", Tags = ["Books"])]
-        public ActionResult<IEnumerable<Book>> GetBooks()
+        IEnumerable<Book> books = bookRepository.GetAll();
+        if (books == null || !books.Any())          
+            return NotFound("No books in database");
+        
+        return Ok(books);
+    }
+
+    // GET: api/Books/5
+    [HttpGet("{id}")]
+    [ProducesResponseType(200, Type = typeof(Book))]
+    [ProducesResponseType(400, Type = typeof(BadRequest))]
+    [ProducesResponseType(404, Type = typeof(NotFound))]
+    [SwaggerOperation(Summary = "Get a book by Id", Tags = ["Books"])]
+    public ActionResult<Book> GetBook(int id)
+    {
+        if (id <= 0)
+            return BadRequest();
+
+        if (!bookRepository.BookExists(id))
+            return NotFound($"Book with id {id} does not exist");
+
+        Book book = bookRepository.GetById(id);
+        return Ok(book);
+    }
+
+    // POST: api/Books
+    [HttpPost]
+    [ProducesResponseType(201, Type = typeof(Created))]
+    [ProducesResponseType(400, Type = typeof(BadRequest))]
+    [SwaggerOperation(Summary = "Create a book", Tags = ["Books"])]
+    public ActionResult<Book> CreateBook([FromBody] BookDto bookRequest)
+    {
+        if (!ModelState.IsValid)
+            return BadRequest(ModelState);
+
+        Book book = new()
         {
-            IEnumerable<Book> books = bookRepository.GetAll();
-            if (books == null || !books.Any())          
-                return NotFound("No books in database");
+            Author = bookRequest.Author,
+            Name = bookRequest.Name,
+            AvailableCopies = bookRequest.TotalAvailableCopies,
+            TotalAvailableCopies = bookRequest.TotalAvailableCopies,
+            NumberOfPages = bookRequest.NumberOfPages,
+            ISBN = bookRequest.ISBN
+        };
+
+        bookRepository.Create(book);
+        return CreatedAtAction("GetBook", new { id = book.Id }, book);
+    }
+
+    // PUT: api/Books/5
+    [HttpPut("{id}")]
+    [ProducesResponseType(200, Type = typeof(OkResult))]
+    [ProducesResponseType(400, Type = typeof(BadRequest))]
+    [ProducesResponseType(404, Type = typeof(NotFound))]
+    [SwaggerOperation(Summary = "Update a book", Tags = ["Books"])]
+    public IActionResult UpdateBook(int id, [FromBody] BookDto bookRequest)
+    {
+        if (!bookRepository.BookExists(id))
+        {
+            return NotFound($"Book with id {id} does not exist");
+        }
             
-            return Ok(books);
-        }
+        if (!ModelState.IsValid)
+            return BadRequest(ModelState);
 
-        // GET: api/Books/5
-        [HttpGet("{id}")]
-        [ProducesResponseType(200, Type = typeof(Book))]
-        [ProducesResponseType(400, Type = typeof(BadRequest))]
-        [ProducesResponseType(404, Type = typeof(NotFound))]
-        [SwaggerOperation(Summary = "Get a book by Id", Tags = ["Books"])]
-        public ActionResult<Book> GetBook(int id)
+        Book book = bookRepository.GetById(id);
         {
-            if (id <= 0)
-                return BadRequest();
+            book.Author = bookRequest.Author;
+            book.Name = bookRequest.Name;
+            book.AvailableCopies = bookRequest.TotalAvailableCopies;
+            book.TotalAvailableCopies = bookRequest.TotalAvailableCopies;
+            book.NumberOfPages = bookRequest.NumberOfPages;
+            book.ISBN = bookRequest.ISBN;
+        };
 
-            if (!bookRepository.BookExists(id))
-                return NotFound($"Book with id {id} does not exist");
-
-            Book book = bookRepository.GetById(id);
-            return Ok(book);
-        }
-
-        // POST: api/Books
-        [HttpPost]
-        [ProducesResponseType(201, Type = typeof(Created))]
-        [ProducesResponseType(400, Type = typeof(BadRequest))]
-        [SwaggerOperation(Summary = "Create a book", Tags = ["Books"])]
-        public ActionResult<Book> CreateBook([FromBody] BookDto bookRequest)
+        if (!rentalEntryRepository.RentalEntryByTitleIdExist(id))
         {
-            if (!ModelState.IsValid)
-                return BadRequest(ModelState);
-
-            Book book = new()
-            {
-                Author = bookRequest.Author,
-                Name = bookRequest.Name,
-                AvailableCopies = bookRequest.TotalAvailableCopies,
-                TotalAvailableCopies = bookRequest.TotalAvailableCopies,
-                NumberOfPages = bookRequest.NumberOfPages,
-                ISBN = bookRequest.ISBN
-            };
-
-            bookRepository.Create(book);
-            return CreatedAtAction("GetBook", new { id = book.Id }, book);
+            bookRepository.Update(book);
         }
-
-        // PUT: api/Books/5
-        [HttpPut("{id}")]
-        [ProducesResponseType(200, Type = typeof(OkResult))]
-        [ProducesResponseType(400, Type = typeof(BadRequest))]
-        [ProducesResponseType(404, Type = typeof(NotFound))]
-        [SwaggerOperation(Summary = "Update a book", Tags = new[] { "Books" })]
-        public IActionResult UpdateBook(int id, [FromBody] BookDto bookRequest)
+        else
         {
-            if (!bookRepository.BookExists(id))
-            {
-                return NotFound($"Book with id {id} does not exist");
-            }
-                
-            if (!ModelState.IsValid)
-                return BadRequest(ModelState);
-
-            Book book = bookRepository.GetById(id);
-            {
-                book.Author = bookRequest.Author;
-                book.Name = bookRequest.Name;
-                book.AvailableCopies = bookRequest.TotalAvailableCopies;
-                book.TotalAvailableCopies = bookRequest.TotalAvailableCopies;
-                book.NumberOfPages = bookRequest.NumberOfPages;
-                book.ISBN = bookRequest.ISBN;
-            };
-
-            if (!rentalEntryRepository.RentalEntryByTitleIdExist(id))
-            {
-                bookRepository.Update(book);
-            }
-            else
-            {
-                return BadRequest("This title was found in rentals. This title cannot be updated");
-            }
-
-            return Ok($"Book with id {id} was successfully updated");
+            return BadRequest("This title was found in rentals. This title cannot be updated");
         }
 
-        // DELETE: api/Books/5
-        [HttpDelete("{id}")]
-        [ProducesResponseType(200, Type = typeof(OkResult))]
-        [ProducesResponseType(400, Type = typeof(BadRequest))]
-        [ProducesResponseType(404, Type = typeof(NotFound))]
-        [SwaggerOperation(Summary = "Delete a book by Id", Tags = ["Books"])]
-        public IActionResult DeleteBook(int id)
+        return Ok($"Book with id {id} was successfully updated");
+    }
+
+    // DELETE: api/Books/5
+    [HttpDelete("{id}")]
+    [ProducesResponseType(200, Type = typeof(OkResult))]
+    [ProducesResponseType(400, Type = typeof(BadRequest))]
+    [ProducesResponseType(404, Type = typeof(NotFound))]
+    [SwaggerOperation(Summary = "Delete a book by Id", Tags = ["Books"])]
+    public IActionResult DeleteBook(int id)
+    {
+        if (!bookRepository.BookExists(id))
+            return NotFound($"Book with id {id} does not exist");
+
+        if (!rentalEntryRepository.RentalEntryByTitleIdExist(id))
         {
-            if (!bookRepository.BookExists(id))
-                return NotFound($"Book with id {id} does not exist");
-
-            if (!rentalEntryRepository.RentalEntryByTitleIdExist(id))
-            {
-                bookRepository.Delete(id);
-            }
-            else
-            {
-                return BadRequest($"This title was found in rentals. This title cannot be removed");
-            }
-
-            return Ok($"Book with id {id} was successfully deleted");
+            bookRepository.Delete(id);
         }
+        else
+        {
+            return BadRequest($"This title was found in rentals. This title cannot be removed");
+        }
+
+        return Ok($"Book with id {id} was successfully deleted");
     }
 }
--- LibraryAppWebAPI/Controllers/MessagesController.cs
+++ LibraryAppWebAPI/Controllers/MessagesController.cs
@@ -6,61 +6,60 @@ using LibraryAppWebAPI.Models;
 using LibraryAppWebAPI.Repository.Interfaces;
 using LibraryAppWebAPI.Service.IServices;
 
-namespace LibraryAppWebAPI.Controllers
+namespace LibraryAppWebAPI.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+[SwaggerTag("Messages")]
+public class MessagesController(IMessageRepository messageRepository, IMemberRepository memberRepository, IMessagingService messagingService) : ControllerBase
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    [SwaggerTag("Messages")]
-    public class MessagesController(IMessageRepository messageRepository, IMemberRepository memberRepository, IMessagingService messagingService) : ControllerBase
+    // GET: api/Messages
+    [HttpGet]
+    [ProducesResponseType(200, Type = typeof(OkResult))]
+    [ProducesResponseType(404, Type = typeof(NotFound))]
+    [SwaggerOperation(Summary = "Get all messages", Tags = ["Messages"])]
+    public ActionResult<IEnumerable<Message>> GetMessages()
     {
-        // GET: api/Messages
-        [HttpGet]
-        [ProducesResponseType(200, Type = typeof(OkResult))]
-        [ProducesResponseType(404, Type = typeof(NotFound))]
-        [SwaggerOperation(Summary = "Get all messages", Tags = ["Messages"])]
-        public ActionResult<IEnumerable<Message>> GetMessages()
-        {
-            IEnumerable<Message> messages = messageRepository.GetAll();
+        IEnumerable<Message> messages = messageRepository.GetAll();
 
-            if (messages == null || !messages.Any())
-                return NotFound("No messages in database");
+        if (messages == null || !messages.Any())
+            return NotFound("No messages in database");
 
-            return Ok(messages);
-        }
+        return Ok(messages);
+    }
 
-        // GET: api/Messages/5
-        [HttpGet("{id}")]
-        [ProducesResponseType(200, Type = typeof(Message))]
-        [ProducesResponseType(400, Type = typeof(BadRequest))]
-        [ProducesResponseType(404, Type = typeof(NotFound))]
-        [SwaggerOperation(Summary = "Get message by id", Tags = ["Messages"])]
-        public ActionResult<Message> GetMessage(int id)
-        {
-            if (!messageRepository.MessageExists(id))
-                return NotFound($"Message with id {id} does not exist");
+    // GET: api/Messages/5
+    [HttpGet("{id}")]
+    [ProducesResponseType(200, Type = typeof(Message))]
+    [ProducesResponseType(400, Type = typeof(BadRequest))]
+    [ProducesResponseType(404, Type = typeof(NotFound))]
+    [SwaggerOperation(Summary = "Get message by id", Tags = ["Messages"])]
+    public ActionResult<Message> GetMessage(int id)
+    {
+        if (!messageRepository.MessageExists(id))
+            return NotFound($"Message with id {id} does not exist");
 
-            Message message = messageRepository.GetById(id);
-            return Ok(message);
-        }
+        Message message = messageRepository.GetById(id);
+        return Ok(message);
+    }
 
-        // GET: api/Messages/Member/5
-        [HttpGet("Member/{userId}")]
-        [ProducesResponseType(200, Type = typeof(List<Message>))]
-        [ProducesResponseType(400, Type = typeof(BadRequest))]
-        [ProducesResponseType(404, Type = typeof(NotFoundResult))]
-        [SwaggerOperation(Summary = "Get messages for specific user by user Id", Tags = ["Messages"])]
-        public ActionResult<List<Message>> GetMessagesByUserId(int userId)
-        {
-            if (!memberRepository.MemberExists(userId))
-                return NotFound($"Member with id {userId} does not exist");
+    // GET: api/Messages/Member/5
+    [HttpGet("Member/{userId}")]
+    [ProducesResponseType(200, Type = typeof(List<Message>))]
+    [ProducesResponseType(400, Type = typeof(BadRequest))]
+    [ProducesResponseType(404, Type = typeof(NotFoundResult))]
+    [SwaggerOperation(Summary = "Get messages for specific user by user Id", Tags = ["Messages"])]
+    public ActionResult<List<Message>> GetMessagesByUserId(int userId)
+    {
+        if (!memberRepository.MemberExists(userId))
+            return NotFound($"Member with id {userId} does not exist");
 
-            Member member = memberRepository.GetById(userId);
+        Member member = memberRepository.GetById(userId);
 
-            List<Message> messages = messagingService.GetMessagesForUser(userId);
-            if (messages == null || !messages.Any())
-                return NotFound($"No messages for {member.FullName()}");
+        List<Message> messages = messagingService.GetMessagesForUser(userId);
+        if (messages == null || !messages.Any())
+            return NotFound($"No messages for {member.FullName()}");
 
-            return Ok(messages);
-        }
+        return Ok(messages);
     }
 }
