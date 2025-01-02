2e5297ec6c375e983a459a2f74bc0fd85e18bcbb
Matus Benus <matusbenus02@gmail.com>
2025-01-02 18:03:56 +0100

    added a migration for seeding database with data and created a background job for deleting data at 1 a.m. every day.

---
+++ LibraryAppWebAPI/CleanUp/DataCleanupJob.cs
@@ -0,0 +1,29 @@
+﻿using LibraryAppWebAPI.DataContext;
+using Microsoft.EntityFrameworkCore;
+
+namespace LibraryAppWebAPI.CleanUp;
+
+public class DataCleanupJob
+{
+    private readonly LibraryContext _libraryContext;
+
+    public DataCleanupJob(LibraryContext libraryContext)
+    {
+        _libraryContext = libraryContext;
+    }
+
+    public async Task ExecuteAsync()
+    {
+        Console.WriteLine("Začína premazávanie dát...\n");
+
+        await _libraryContext.Database.ExecuteSqlRawAsync(@"
+            DELETE FROM Members;
+            DELETE FROM Messages;
+            DELETE FROM QueueItems;
+            DELETE FROM RentalEntries;
+            DELETE FROM Title;
+        ");
+
+        Console.WriteLine("\nPremazávanie dát dokončené.");
+    }
+}
--- LibraryAppWebAPI/Migrations/20250102165956_Initial.cs
+++ LibraryAppWebAPI/Migrations/20250102165956_Initial.cs
@@ -3,6 +3,8 @@ using Microsoft.EntityFrameworkCore.Migrations;
 
 #nullable disable
 
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
 namespace LibraryAppWebAPI.Migrations
 {
     /// <inheritdoc />
@@ -127,6 +129,54 @@ namespace LibraryAppWebAPI.Migrations
                         principalTable: "Title",
                         principalColumn: "Id",
                         onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Members",
+                columns: new[] { "Id", "DateOfBirth", "FirstName", "LastName", "PersonalId" },
+                values: new object[,]
+                {
+                    { 1, new DateTime(1990, 5, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), "John", "Doe", "123456789" },
+                    { 2, new DateTime(1985, 7, 12, 0, 0, 0, 0, DateTimeKind.Unspecified), "Jane", "Smith", "987654321" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Title",
+                columns: new[] { "Id", "Author", "AvailableCopies", "Discriminator", "ISBN", "Name", "NumberOfPages", "TotalAvailableCopies" },
+                values: new object[,]
+                {
+                    { 1, "George Orwell", 5, "Book", "978-0451524935", "1984", 328, 5 },
+                    { 2, "Aldous Huxley", 3, "Book", "978-0060850524", "Brave New World", 268, 3 }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Title",
+                columns: new[] { "Id", "Author", "AvailableCopies", "Discriminator", "Name", "NumberOfMinutes", "PublishYear", "TotalAvailableCopies" },
+                values: new object[,]
+                {
+                    { 3, "Christopher Nolan", 4, "Dvd", "Inception", 148, 2010, 4 },
+                    { 4, "Steven Spielberg", 2, "Dvd", "Jurassic Park", 127, 1993, 2 }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Messages",
+                columns: new[] { "Id", "MemberId", "MessageContext", "MessageSubject", "SendData" },
+                values: new object[,]
+                {
+                    { 1, 1, "Dear John Doe, we are delighted to welcome you to our library. Explore our collection and enjoy our services!", "Welcome to the Library!", new DateTime(2025, 1, 2, 16, 59, 55, 689, DateTimeKind.Utc).AddTicks(7302) },
+                    { 2, 2, "Dear Jane Smith, we are delighted to welcome you to our library. Explore our collection and enjoy our services!", "Welcome to the Library!", new DateTime(2025, 1, 2, 16, 59, 55, 689, DateTimeKind.Utc).AddTicks(7306) }
+                });
+
+            migrationBuilder.InsertData(
+                table: "RentalEntries",
+                columns: new[] { "Id", "MaxReturnDate", "MemberId", "RentedDate", "ReturnDate", "TimesProlongued", "TitleId", "TitleType" },
+                values: new object[,]
+                {
+                    { 1, new DateTime(2025, 1, 16, 16, 59, 55, 689, DateTimeKind.Utc).AddTicks(7264), 1, new DateTime(2024, 12, 26, 16, 59, 55, 689, DateTimeKind.Utc).AddTicks(7257), null, 0, 1, 1 },
+                    { 2, new DateTime(2025, 2, 2, 16, 59, 55, 689, DateTimeKind.Utc).AddTicks(7270), 2, new DateTime(2024, 12, 23, 16, 59, 55, 689, DateTimeKind.Utc).AddTicks(7270), null, 1, 2, 1 },
+                    { 3, new DateTime(2025, 1, 8, 16, 59, 55, 689, DateTimeKind.Utc).AddTicks(7272), 1, new DateTime(2025, 1, 2, 16, 59, 55, 689, DateTimeKind.Utc).AddTicks(7271), new DateTime(2025, 1, 7, 16, 59, 55, 689, DateTimeKind.Utc).AddTicks(7272), 0, 3, 2 },
+                    { 4, new DateTime(2025, 1, 5, 16, 59, 55, 689, DateTimeKind.Utc).AddTicks(7274), 2, new DateTime(2024, 12, 29, 16, 59, 55, 689, DateTimeKind.Utc).AddTicks(7274), new DateTime(2025, 1, 3, 16, 59, 55, 689, DateTimeKind.Utc).AddTicks(7275), 0, 4, 2 },
+                    { 5, new DateTime(2024, 12, 13, 16, 59, 55, 689, DateTimeKind.Utc).AddTicks(7276), 1, new DateTime(2024, 12, 3, 16, 59, 55, 689, DateTimeKind.Utc).AddTicks(7276), new DateTime(2024, 12, 15, 16, 59, 55, 689, DateTimeKind.Utc).AddTicks(7277), 0, 1, 1 }
                 });
 
             migrationBuilder.CreateIndex(
